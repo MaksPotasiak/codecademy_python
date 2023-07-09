8a1299ff327d22bcbe136c3462cac6bba0419bb8
MaksPotasiak <maks-pot@hotmail.com>
2023-07-09 16:18:18 +0100

    Added input checker

--- dice_roller_project/dice_roller_project.py
+++ dice_roller_project/dice_roller_project.py
@@ -8,13 +8,13 @@ def input_checker(user_input):
     else:
         return False
 
-
+# roll_dice generates rolls nad puts them into a list.
 def roll_dice(number_of_dice, size_of_dice):
-    rolls = []
-    for i in range(number_of_dice):
-        rolls.append(random.randint(1, size_of_dice))
-    overall_result = sum(rolls)
-    print rolls
-    return overall_result
+    while input_checker(number_of_dice) and input_checker(size_of_dice):
+        rolls = []
+        for i in range(int(number_of_dice)):
+            rolls.append(random.randint(1, int(size_of_dice)))
+        return rolls 
+    return "Invalid input"
 
-print roll_dice(2, 8)
+print roll_dice("4", "8")
